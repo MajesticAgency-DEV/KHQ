ba717b2bcdfbe891a13e639549f758402f7c8ce6
Mutaz Tbakhi <Mutazaltbakhi900@gmail.com>
2025-08-24 23:51:45 +0300

    database

---
+++ KHQ.Repo/Migrations/20250824205021_AboutUs_Stains.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KHQ.Repo.Migrations
+{
+    /// <inheritdoc />
+    public partial class AboutUs_Stains : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "ab369bd0-fd1a-4647-bb60-f1f6bea14f18");
+
+            migrationBuilder.CreateTable(
+                name: "AboutUs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TitleEn = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TitleAr = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DescriptionEn = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DescriptionAr = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AboutUs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SocialMedia",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    WhatsAppLink = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    InstaLink = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FaceLink = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    WebSiteLink = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SocialMedia", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StainDetails",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TitleEn = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TitleAr = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DescriptionEn = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DescriptionAr = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StainsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StainDetails", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Stains",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    NameEn = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NameAr = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stains", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "9f06dfbc-6304-4daa-a2b1-95f9b280d227", null, "User", "User" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "a18be9c0",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "e84ecab0-75a6-4ede-9e51-a427fbde0ec2", "AQAAAAIAAYagAAAAEJGtn2AKoaDYJlgung243+KPn9j+CjpWvS4c6fonrZk/Z/WXk9HwzAPL0WtpjBD92g==" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AboutUs");
+
+            migrationBuilder.DropTable(
+                name: "SocialMedia");
+
+            migrationBuilder.DropTable(
+                name: "StainDetails");
+
+            migrationBuilder.DropTable(
+                name: "Stains");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "9f06dfbc-6304-4daa-a2b1-95f9b280d227");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "ab369bd0-fd1a-4647-bb60-f1f6bea14f18", null, "User", "User" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "a18be9c0",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "b07b8b74-e7d7-4ace-9864-a0092bc27888", "AQAAAAIAAYagAAAAEJVhHAkB88D3A+XzNmJ7eES1Llie1A1mlieD/R8l2BBsYFq3DPnu/nVuJyeTPnSkBA==" });
+        }
+    }
+}
