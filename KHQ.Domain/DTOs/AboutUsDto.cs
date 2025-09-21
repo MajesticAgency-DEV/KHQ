5ca87409c7e9d1bb4fe13d146d2eb301644a2ac4
Mutaz Tbakhi <Mutazaltbakhi900@gmail.com>
2025-09-21 23:41:08 +0300

    Aboutus

--- KHQ.Domain/DTOs/AboutUsDto.cs
+++ KHQ.Domain/DTOs/AboutUsDto.cs
@@ -5,7 +5,7 @@
         public Guid Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public H_AboutUsDto H_AboutUsDto { get; set; }
+        public IEnumerable<H_AboutUsDto> H_AboutUsDto { get; set; }
         public string? CoverPhoto { get; set; }
         public string? AboutUsImage { get; set; }
     }
