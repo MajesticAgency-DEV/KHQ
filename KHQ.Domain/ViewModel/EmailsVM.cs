3577071fee17467d2f3f38c2c354e6a4f928e0b4
Ola Alshloul <ola.alshloul@omicron-jo.com>
2025-10-06 13:20:30 +0300

    Add email page in portal

--- KHQ.Domain/ViewModel/EmailsVM.cs
+++ KHQ.Domain/ViewModel/EmailsVM.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace KHQ.Domain.ViewModel
+﻿namespace KHQ.Domain.ViewModel
 {
     public class EmailsVM
     {
